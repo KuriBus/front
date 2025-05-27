0cf8dbdcfee90ff262632cc72a223943dc119a8c
202015027hhj <amelichano@naver.com>
2025-05-27 20:19:58 +0900

    /refactor

--- game.js
+++ game.js
@@ -1,22 +1,3 @@
-// import MainScene from './mainScene.js';
-// import LoginScene from './loginScene.js';
-
-// const config = {
-//   type: Phaser.AUTO,
-//   width: 1600,
-//   height: 900,
-//   parent: 'game-container',
-//   physics: {
-//     default: 'arcade',
-//     arcade: { debug: false }
-//   },
-//   dom: {
-//     createContainer: true
-//   },
-//   scene: [LoginScene, MainScene]
-// };
-
-// const game = new Phaser.Game(config);
 import StartScene from './StartScene.js';
 import NicknameScene from './NicknameScene.js';
 import CharacterSelectScene from './CharacterSelectScene.js';
